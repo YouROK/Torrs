1beff954313f1fd03bd1158d299aeb7b2a53abb2
yourok <8yourok8@mail.ru>
2024-03-05 22:48:34 +0300

    remove ssl, add cors and port

--- web/server.go
+++ web/server.go
@@ -1,45 +1,54 @@
 package web
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/acme/autocert"
 	"log"
 	"path/filepath"
 	"torrsru/db/sync"
 	"torrsru/web/api"
-	"torrsru/web/gincert"
 	"torrsru/web/global"
 	"torrsru/web/static"
 )
 
-func Start() {
+func Start(port string) {
 	go sync.StartSync()
 
 	//gin.SetMode(gin.DebugMode)
 	//gin.SetMode(gin.ReleaseMode)
+
+	corsCfg := cors.DefaultConfig()
+	corsCfg.AllowAllOrigins = true
+	corsCfg.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "X-Requested-With", "Accept", "Authorization"}
+
 	global.Route = gin.New()
-	global.Route.Use(gin.Recovery())
+	global.Route.Use(gin.Recovery(), cors.New(corsCfg))
 	static.RouteStaticFiles(global.Route)
 	global.Route.LoadHTMLGlob(filepath.Join(global.PWD, "views/*.go.html"))
 	api.SetRoutes(global.Route)
 
-	if gin.Mode() == gin.DebugMode {
-		err := global.Route.Run(":80")
-		if err != nil {
-			log.Println("Error start server:", err)
-		}
-	} else {
-		am := autocert.Manager{
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist("torrs.ru"),
-			Cache:      autocert.DirCache(filepath.Join(global.PWD, "cache")),
-		}
-
-		err := gincert.RunWithManager(global.Route, &am)
-		if err != nil {
-			log.Println("Error start server:", err)
-		}
+	err := global.Route.Run(":" + port)
+	if err != nil {
+		log.Println("Error start server:", err)
 	}
+
+	//if gin.Mode() == gin.DebugMode {
+	//	err := global.Route.Run(":80")
+	//	if err != nil {
+	//		log.Println("Error start server:", err)
+	//	}
+	//} else {
+	//	am := autocert.Manager{
+	//		Prompt:     autocert.AcceptTOS,
+	//		HostPolicy: autocert.HostWhitelist("torrs.ru"),
+	//		Cache:      autocert.DirCache(filepath.Join(global.PWD, "cache")),
+	//	}
+	//
+	//	err := gincert.RunWithManager(global.Route, &am)
+	//	if err != nil {
+	//		log.Println("Error start server:", err)
+	//	}
+	//}
 
 	global.Stoped = true
 }
