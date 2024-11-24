f5ec98348502fc0efe81c4f4b069d68285adbc81
YouROK <8YouROK8@mail.ru>
2024-11-24 17:24:43 +0300

    change args

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"flag"
+	"github.com/alexflint/go-arg"
 	"log"
 	"os"
 	"path/filepath"
@@ -11,18 +11,28 @@ import (
 )
 
 func main() {
+	var args struct {
+		Port         string `default:"8094" arg:"-p" help:"port for http"`
+		RebuildIndex bool   `default:"false" arg:"-r" help:"rebuild index and exit"`
+	}
+	arg.MustParse(&args)
+
 	pwd := filepath.Dir(os.Args[0])
 	pwd, _ = filepath.Abs(pwd)
 	log.Println("PWD:", pwd)
 	global.PWD = pwd
+
 	db.Init()
 
-	port := flag.String("port", "8094", "port for web")
-
-	if port == nil {
-		p := "8094"
-		port = &p
+	if args.RebuildIndex {
+		err := db.RebuildIndex()
+		if err != nil {
+			log.Println("Rebuild index error:", err)
+		} else {
+			log.Println("Rebuild index success")
+		}
+		return
 	}
 
-	web.Start(*port)
+	web.Start(args.Port)
 }
